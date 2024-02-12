f53a33901e59623a97118535d7ee9831f6ea5745
Clawiatura <maksimov19992805@icloud.com>
2024-02-12 12:37:11 +0200

    Class Math2

---
+++ ClassMath2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите значение катета: ");
+double katet = double.Parse(Console.ReadLine());
+Console.Write("Введите значение гипотенузы: ");
+double gipot  = double.Parse(Console.ReadLine());
+double katettoo = Math.Sqrt(gipot * 2 - katet * 2);
+
+Console.WriteLine($"Значение второго катета = {katettoo}");
