c4e6293e0bcfd5ac6772d64f4cf1c63124747ecb
Clawiatura <maksimov19992805@icloud.com>
2024-02-12 12:28:40 +0200

    Добавьте файлы проекта.

---
+++ ClassMath/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите радиус окружности: ");
+double R = double.Parse(Console.ReadLine());
+double Long = 2 * Math.PI * R;
+double S = Math.PI * R * R;
+Console.WriteLine($" Длина окружности - {Long:F2} Площадь окружности - {S:F2} ");
