05008884fb4bb5163583fc27ae21e06a3d2f8782
Clawiatura <maksimov19992805@icloud.com>
2024-02-12 14:10:44 +0200

    YslOperatot

--- YslOperatot/Program.cs
+++ YslOperatot/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Введите число: ");
+﻿using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine());
 Console.Write("Введите число: ");
 int b = int.Parse(Console.ReadLine());
@@ -22,4 +25,38 @@ int i = int.Parse(Console.ReadLine());
 
 Console.WriteLine($"{a} + {b} + {c} + {d} +{f} {v} + {g} +{y} + {u} {i}");
 Console.WriteLine($"{(a + b + c + d + f + v + g +y + u + i) / 2 }");
+{ 
+if (a > 10 && b > 10 && c > 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("10");
+else if (a < 10 && b > 10 && c > 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("9");
+     else if (a < 10 && b < 10 && c > 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("8");
+          else if (a < 10 && b < 10 && c < 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("7");
+               else if (a < 10 && b < 10 && c < 10 && d < 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("6");
+                    else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("5");
+                         else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v < 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("4");
+                              else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v < 10 && g < 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("3");
+                                   else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v < 10 && g < 10 && y < 10 && u > 10 && i > 10) Console.WriteLine("2");
+                                        else if (a < 10 && b < 10 && c < 10 && d < 10 && f <10 && v < 10 && g < 10 && y < 10 && u < 10 && i > 10) Console.WriteLine("1");
+                                             else Console.WriteLine("0");
+}
+{
+    if(a < -10 && b < - 10 && c < -10 && d < - 10 && f < -10 && v < - 10 && g < -10 && y < -10 && u < - 10 && i < -10) Console.WriteLine("0");
+    else if (a > -10 && b < -10 && c < -10 && d < -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("1");
+         else if (a > -10 && b > -10 && c < -10 && d < -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("2");
+              else if (a > -10 && b > -10 && c > -10 && d < -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("3");
+                   else if (a > -10 && b > -10 && c > -10 && d > -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("4");
+                        else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("5");
+                             else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("6");
+                                   else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g > -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("7");
+                                        else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g > -10 && y > -10 && u < -10 && i < -10) Console.WriteLine("8");
+                                             else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g > -10 && y > -10 && u > -10 && i < -10) Console.WriteLine("9");
+                                                  else Console.WriteLine("10");
+}
+
+int min = Math.Min(a, b);
+int min1 = Math.Min(d, c);
+int min2 = Math.Min(min, min1);
+int max = Math.Max(a,b);
+int max1 = Math.Max(d, c);
+int max2 = Math.Max(max1, max);
+Console.WriteLine($"Минимальное значение = {min2} Максимальное значение = {max2}");
 
