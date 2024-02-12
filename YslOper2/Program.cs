065ddf1be5858c096a739a552a06f2166dccaa03
Clawiatura <maksimov19992805@icloud.com>
2024-02-12 13:10:04 +0200

    YslOperator

---
+++ YslOper2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите температуру на улице: ");
+double temp = double.Parse(Console.ReadLine());
+
+if (temp > 20) Console.WriteLine("На прогулку можно выпустить слонов, жирафов и львов.");
+ else if (temp <20) Console.WriteLine("На прогулку можно выпустить медведей, волков и пингвинов");
+       else Console.WriteLine("Можно выпустить только кенгуру");
+{
+     
+}
---
+++ YslOperatot/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int c = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int d = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int f= int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int v = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int g = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int y = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int u = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int i = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"{a} + {b} + {c} + {d} +{f} {v} + {g} +{y} + {u} {i}");
+Console.WriteLine($"{(a + b + c + d + f + v + g +y + u + i) / 2 }");
+
