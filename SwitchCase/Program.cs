d27c62c624c1215501f66661b1db709ccbfeda3f
Clawiatura <maksimov19992805@icloud.com>
2024-02-16 14:49:46 +0200

    SwitchCase

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите операцию: ");
+char oper = char.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+double one = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+double too = double.Parse(Console.ReadLine());
+
+switch (oper)
+{
+    case '+': Console.WriteLine($"{one} + {too} = {one + too}"); break;
+    case '-':Console.WriteLine($"{one} - {too} = {one - too}"); break;
+    case '*': Console.WriteLine($"{one} * {too} = {one * too}"); break;
+    case '/': Console.WriteLine($"{one} / {too} = {one / too}"); break;
+}
--- YslOperatot/Program.cs
+++ YslOperatot/Program.cs
@@ -1,62 +1,64 @@
-﻿using System.ComponentModel.Design;
-using System.Runtime.InteropServices;
+﻿//using System.ComponentModel.Design;
+//using System.Runtime.InteropServices;
 
-Console.Write("Введите число: ");
-int a = int.Parse(Console.ReadLine());
-Console.Write("Введите число: ");
-int b = int.Parse(Console.ReadLine());
-Console.Write("Введите число: ");
-int c = int.Parse(Console.ReadLine());
-Console.Write("Введите число: ");
-int d = int.Parse(Console.ReadLine());
-Console.Write("Введите число: ");
-int f= int.Parse(Console.ReadLine());
-Console.Write("Введите число: ");
-int v = int.Parse(Console.ReadLine());
-Console.Write("Введите число: ");
-int g = int.Parse(Console.ReadLine());
-Console.Write("Введите число: ");
-int y = int.Parse(Console.ReadLine());
-Console.Write("Введите число: ");
-int u = int.Parse(Console.ReadLine());
-Console.Write("Введите число: ");
-int i = int.Parse(Console.ReadLine());
+//Console.Write("Введите число: ");
+//int a = int.Parse(Console.ReadLine());
+//Console.Write("Введите число: ");
+//int b = int.Parse(Console.ReadLine());
+//Console.Write("Введите число: ");
+//int c = int.Parse(Console.ReadLine());
+//Console.Write("Введите число: ");
+//int d = int.Parse(Console.ReadLine());
+//Console.Write("Введите число: ");
+//int f= int.Parse(Console.ReadLine());
+//Console.Write("Введите число: ");
+//int v = int.Parse(Console.ReadLine());
+//Console.Write("Введите число: ");
+//int g = int.Parse(Console.ReadLine());
+//Console.Write("Введите число: ");
+//int y = int.Parse(Console.ReadLine());
+//Console.Write("Введите число: ");
+//int u = int.Parse(Console.ReadLine());
+//Console.Write("Введите число: ");
+//int i = int.Parse(Console.ReadLine());
 
 
-Console.WriteLine($"{a} + {b} + {c} + {d} +{f} {v} + {g} +{y} + {u} {i}");
-Console.WriteLine($"{(a + b + c + d + f + v + g +y + u + i) / 2 }");
-{ 
-if (a > 10 && b > 10 && c > 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("10");
-else if (a < 10 && b > 10 && c > 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("9");
-     else if (a < 10 && b < 10 && c > 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("8");
-          else if (a < 10 && b < 10 && c < 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("7");
-               else if (a < 10 && b < 10 && c < 10 && d < 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("6");
-                    else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("5");
-                         else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v < 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("4");
-                              else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v < 10 && g < 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("3");
-                                   else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v < 10 && g < 10 && y < 10 && u > 10 && i > 10) Console.WriteLine("2");
-                                        else if (a < 10 && b < 10 && c < 10 && d < 10 && f <10 && v < 10 && g < 10 && y < 10 && u < 10 && i > 10) Console.WriteLine("1");
-                                             else Console.WriteLine("0");
-}
-{
-    if(a < -10 && b < - 10 && c < -10 && d < - 10 && f < -10 && v < - 10 && g < -10 && y < -10 && u < - 10 && i < -10) Console.WriteLine("0");
-    else if (a > -10 && b < -10 && c < -10 && d < -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("1");
-         else if (a > -10 && b > -10 && c < -10 && d < -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("2");
-              else if (a > -10 && b > -10 && c > -10 && d < -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("3");
-                   else if (a > -10 && b > -10 && c > -10 && d > -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("4");
-                        else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("5");
-                             else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("6");
-                                   else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g > -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("7");
-                                        else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g > -10 && y > -10 && u < -10 && i < -10) Console.WriteLine("8");
-                                             else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g > -10 && y > -10 && u > -10 && i < -10) Console.WriteLine("9");
-                                                  else Console.WriteLine("10");
-}
+//Console.WriteLine($"{a} + {b} + {c} + {d} +{f} {v} + {g} +{y} + {u} {i}");
+//Console.WriteLine($"{(a + b + c + d + f + v + g +y + u + i) / 2 }");
+//{ 
+//if (a > 10 && b > 10 && c > 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("10");
+//else if (a < 10 && b > 10 && c > 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("9");
+//     else if (a < 10 && b < 10 && c > 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("8");
+//          else if (a < 10 && b < 10 && c < 10 && d > 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("7");
+//               else if (a < 10 && b < 10 && c < 10 && d < 10 && f > 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("6");
+//                    else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v > 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("5");
+//                         else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v < 10 && g > 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("4");
+//                              else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v < 10 && g < 10 && y > 10 && u > 10 && i > 10) Console.WriteLine("3");
+//                                   else if (a < 10 && b < 10 && c < 10 && d < 10 && f < 10 && v < 10 && g < 10 && y < 10 && u > 10 && i > 10) Console.WriteLine("2");
+//                                        else if (a < 10 && b < 10 && c < 10 && d < 10 && f <10 && v < 10 && g < 10 && y < 10 && u < 10 && i > 10) Console.WriteLine("1");
+//                                             else Console.WriteLine("0");
+//}
+//{
+//    if(a < -10 && b < - 10 && c < -10 && d < - 10 && f < -10 && v < - 10 && g < -10 && y < -10 && u < - 10 && i < -10) Console.WriteLine("0");
+//    else if (a > -10 && b < -10 && c < -10 && d < -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("1");
+//         else if (a > -10 && b > -10 && c < -10 && d < -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("2");
+//              else if (a > -10 && b > -10 && c > -10 && d < -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("3");
+//                   else if (a > -10 && b > -10 && c > -10 && d > -10 && f < -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("4");
+//                        else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v < -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("5");
+//                             else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g < -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("6");
+//                                   else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g > -10 && y < -10 && u < -10 && i < -10) Console.WriteLine("7");
+//                                        else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g > -10 && y > -10 && u < -10 && i < -10) Console.WriteLine("8");
+//                                             else if (a > -10 && b > -10 && c > -10 && d > -10 && f > -10 && v > -10 && g > -10 && y > -10 && u > -10 && i < -10) Console.WriteLine("9");
+//                                                  else Console.WriteLine("10");
+//}
 
-int min = Math.Min(a, b);
-int min1 = Math.Min(d, c);
-int min2 = Math.Min(min, min1);
-int max = Math.Max(a,b);
-int max1 = Math.Max(d, c);
-int max2 = Math.Max(max1, max);
-Console.WriteLine($"Минимальное значение = {min2} Максимальное значение = {max2}");
+//int min = Math.Min(a, b);
+//int min1 = Math.Min(d, c);
+//int min2 = Math.Min(min, min1);
+//int max = Math.Max(a,b);
+//int max1 = Math.Max(d, c);
+//int max2 = Math.Max(max1, max);
+//Console.WriteLine($"Минимальное значение = {min2} Максимальное значение = {max2}");
+
+
 
